8a8d8d55bcb4efe00dbb84c4a9d2fccd6ac47f53
DAGGOTH\massi <massimobono1@gmail.com>
2016-11-26 11:10:43 +0100

    PLAYERHANDLING
    - included child fxml inside root;

--- src/com/massimobono/podiliardino/Main.java
+++ src/com/massimobono/podiliardino/Main.java
@@ -1,18 +1,23 @@
 package com.massimobono.podiliardino;
 	
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
+import java.util.Optional;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 
 
 public class Main extends Application {
 	
 	private Stage primaryStage;
-	private Scene rootScene;
+	private BorderPane rootScene;
 	
 	public static void main(String[] args) {
 		launch(args);
@@ -29,6 +34,7 @@ public class Main extends Application {
 		
 		try {
 			this.loadAndShowRootLayout();
+			this.setMainTo("PlayerHandling");
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
@@ -38,10 +44,28 @@ public class Main extends Application {
 	private void loadAndShowRootLayout() throws IOException {
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(this.getClass().getResource("view/RootLayout.fxml"));
-		this.rootScene = new Scene(loader.load());
+		this.rootScene = (BorderPane)loader.load();
 		this.rootScene.getStylesheets().add(this.getClass().getResource("application.css").toExternalForm());
-		this.primaryStage.setScene(this.rootScene);
+		this.primaryStage.setScene(new Scene(this.rootScene));
 		this.primaryStage.show();
+	}
+	
+	private void setMainTo(String fxmlFile) throws IOException {
+		
+		if (!fxmlFile.endsWith(".fxml")) {
+			fxmlFile +=".fxml";
+		}
+		FXMLLoader loader = new FXMLLoader();
+		String baseUrl = String.format("view/%s", fxmlFile);
+		Optional<URL> resourceURL = Optional.of(this.getClass().getResource(baseUrl));
+		if (!resourceURL.isPresent()) {
+			throw new FileNotFoundException(String.format("Couldn't find %s", baseUrl));
+		} else {
+			loader.setLocation(resourceURL.get());
+			this.rootScene.setCenter(loader.load());
+		}
+		
+		
 	}
 	
 }
